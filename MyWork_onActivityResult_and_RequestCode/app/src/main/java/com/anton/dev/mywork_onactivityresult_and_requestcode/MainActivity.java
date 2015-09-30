6889c1106880caaa772b4365aa73491b986bad39
unknown <User04@DE02-WKS-004.de02.local>
2015-09-30 11:57:49 +0300

    Создан проект onActivityResult и RequestCode кода пока нет

---
+++ MyWork_onActivityResult_and_RequestCode/app/src/main/java/com/anton/dev/mywork_onactivityresult_and_requestcode/MainActivity.java
@@ -0,0 +1,12 @@
+package com.anton.dev.mywork_onactivityresult_and_requestcode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
