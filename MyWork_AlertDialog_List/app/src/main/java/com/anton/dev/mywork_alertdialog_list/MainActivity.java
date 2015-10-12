7b06e6a1eacecdf7513550ad188f1a3e1fbe25eb
antonlazev <lazev.anton@gmail.com>
2015-10-12 15:40:05 +0300

    Создан массив PHONES,создан title и в drawable добавлена иконка для диалога

--- MyWork_AlertDialog_List/app/src/main/java/com/anton/dev/mywork_alertdialog_list/MainActivity.java
+++ MyWork_AlertDialog_List/app/src/main/java/com/anton/dev/mywork_alertdialog_list/MainActivity.java
@@ -1,13 +1,25 @@
 package com.anton.dev.mywork_alertdialog_list;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private Button btn_Phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_Phone = (Button)findViewById(R.id.btn_cheak_phone);
+    }
+    public void pushBTN(View v){
+        /*Создаем массив с моделя телефонов*/
+        final String[] PHONES = {"IPHONE","NOKIA","SAMSUNG","HTC","MEIZU","ONE PLUS"};
+        AlertDialog.Builder alertdialog = new AlertDialog.Builder(MainActivity.this);
+        alertdialog.setTitle("Выберите модель телефона")
+                .setIcon(R.drawable.ic_developer_mode_black_24dp)
+                .setCancelable(false)
     }
 }
