c37a74b0b6e870265dfcf81a77a92bd7870738d2
antonlazev <lazev.anton@gmail.com>
2015-11-16 15:55:32 +0300

    Добавил toogle button и TextView

--- MyWork_ToggleButton/app/src/main/java/com/anton/dev/mywork_togglebutton/MainActivity.java
+++ MyWork_ToggleButton/app/src/main/java/com/anton/dev/mywork_togglebutton/MainActivity.java
@@ -1,13 +1,33 @@
 package com.anton.dev.mywork_togglebutton;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+    private TextView mtext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ToggleButton mButton = (ToggleButton)findViewById(R.id.button);
+        mButton.setOnCheckedChangeListener(this);
+
+    }
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked){
+            mtext.setText("Button Cheked");
+        }
+        else{
+            mtext.setText("Button unchecked");
+        }
+
     }
 }
