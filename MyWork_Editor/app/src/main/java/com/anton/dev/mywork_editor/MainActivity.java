6988203bf4ed111b13c5fcde24a58920241f6498
antonlazev <lazev.anton@gmail.com>
2015-11-17 11:40:52 +0300

    New Project

---
+++ MyWork_Editor/app/src/main/java/com/anton/dev/mywork_editor/MainActivity.java
@@ -0,0 +1,19 @@
+package com.anton.dev.mywork_editor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
+
+
