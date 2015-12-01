efc9d1d6bce0ec67438dd0a599bbb66a2a4cdb7c
antonlazev <lazev.anton@gmail.com>
2015-12-01 16:59:50 +0300

    Переписал (Тренировка)

--- MyWorkTabs/app/src/main/java/com/anton/dev/myworktabs/MainActivity.java
+++ MyWorkTabs/app/src/main/java/com/anton/dev/myworktabs/MainActivity.java
@@ -5,13 +5,37 @@ import android.os.Bundle;
 import android.widget.TabHost;
 
 public class MainActivity extends Activity {
+    private TabHost tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TabHost tab = (TabHost)findViewById(R.id.tabHost);
-        /*Инициализация контейнера вкладок*/
-        tab.setup();
+        tabs = (TabHost) findViewById(R.id.tabhost);
+
+        /*setup() — инициализирует контейнер вкладок.
+        Необходимо вызывать перед добавлением вкладок,
+        если TabHost загружается методом findViewById();*/
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+        /*Первая вкладка*/
+        spec.setContent(R.id.tabPage1);
+        spec.setIndicator("Document 1");
+        tabs.addTab(spec);
+
+        /*Вторая вкладка*/
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tabPage2);
+        spec.setIndicator("Document 2");
+        tabs.addTab(spec);
+
+        /*Третья вкладка*/
+        spec = tabs.newTabSpec("tag3");
+        spec.setContent(R.id.tabPage3);
+        spec.setIndicator("Document 3");
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
     }
 }
