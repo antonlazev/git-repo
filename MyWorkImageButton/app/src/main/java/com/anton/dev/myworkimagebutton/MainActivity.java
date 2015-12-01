d63c6c394c4dcfa6b37a6a4349086e79a034632a
antonlazev <lazev.anton@gmail.com>
2015-12-01 15:34:52 +0300

    Работа с ImageButton

---
+++ MyWorkImageButton/app/src/main/java/com/anton/dev/myworkimagebutton/MainActivity.java
@@ -0,0 +1,33 @@
+package com.anton.dev.myworkimagebutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+    private ImageButton mButton;
+    private boolean mPlay = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (ImageButton)findViewById(R.id.imageButton);
+        mButton.setImageResource(R.drawable.play);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPlay){
+                    mButton.setImageResource(R.drawable.pause);
+                }else{
+                    mButton.setImageResource(R.drawable.play);
+                }
+                mPlay = !mPlay;
+            }
+        });
+
+    }
+
+
+}
