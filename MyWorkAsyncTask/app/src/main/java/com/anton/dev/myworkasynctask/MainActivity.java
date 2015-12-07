8dfe7e444833208f29c8422e1432ca6ad59fb253
antonlazev <lazev.anton@gmail.com>
2015-12-07 17:18:31 +0300

    class AsynkTask

--- MyWorkAsyncTask/app/src/main/java/com/anton/dev/myworkasynctask/MainActivity.java
+++ MyWorkAsyncTask/app/src/main/java/com/anton/dev/myworkasynctask/MainActivity.java
@@ -1,7 +1,9 @@
 package com.anton.dev.myworkasynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public class MyWorkAsynctask extends AsyncTask<Void, Integer,Void>{
+
+        @Override
+        //Метод вызывается в потоке пользовательского интерфейса
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getApplicationContext(),"Начало процесcа",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
     }
 }
